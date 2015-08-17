2490ac19a54de0cdb6af94879da4f6936f2ee7e8
Paul Hawxby <phawxby@gmail.com>
2015-08-17 13:57:55 +0100

    Country selector JS

---
+++ js/modules/country-selector.js
@@ -0,0 +1,22 @@
+(function(window, document) {
+
+	var countrySelector = document.getElementById("xrx_bnrv4_header_country_selector");
+	if(countrySelector && countrySelector.children)
+	{
+		for(var i = 0; i < countrySelector.children.length; i++)
+		{
+			var child = countrySelector.children[i];
+
+			if (child && child.nodeName === "A")
+			{
+				var href = child.getAttribute("href");
+
+				if (href && href.match(/.html$/i))
+				{
+					child.setAttribute("href", href + "?url=" + encodeURIComponent(window.location.href));
+				}
+			}
+		}
+	}
+
+})(window, document);
