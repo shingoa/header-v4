97bc9a1bbb4086e92670e11d0bfbcd0370a9dc22
Paul Hawxby <phawxby@gmail.com>
2015-08-20 14:43:12 +0100

    Character fixes for IE8

--- js/xrx_bnrv4.ie8.js
+++ js/xrx_bnrv4.ie8.js
@@ -1,4 +1,11 @@
 "use strict";
 
-//=require xrx_bnrv4.js
-//=require modules/ie8.js
+//=include modules/_helpers.js
+//=include modules/_metrics.js
+//=include modules/intershop.js
+//=include modules/country-selector.js
+//=include modules/tracking.js
+//=include modules/search.js
+//=include modules/header.js
+
+//=include modules/ie8.js
--- js/xrx_bnrv4.js
+++ js/xrx_bnrv4.js
@@ -1,9 +1,9 @@
 "use strict";
 
-//=require modules/_helpers.js
-//=require modules/_metrics.js
-//=require modules/intershop.js
-//=require modules/country-selector.js
-//=require modules/tracking.js
-//=require modules/search.js
-//=require modules/header.js
+//=include modules/_helpers.js
+//=include modules/_metrics.js
+//=include modules/intershop.js
+//=include modules/country-selector.js
+//=include modules/tracking.js
+//=include modules/search.js
+//=include modules/header.js
